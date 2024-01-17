ac4797ed5df339657397f15a73b8ced23df43107
Chellapandi K <chellapandi51@gmail.com>
2024-01-18 01:29:11 +0530

    create account page design added

--- app/src/main/java/com/dreamfutureone/milkmanui/ui/account/CreateAccountActivity.java
+++ app/src/main/java/com/dreamfutureone/milkmanui/ui/account/CreateAccountActivity.java
@@ -1,9 +1,12 @@
 package com.dreamfutureone.milkmanui.ui.account;
 
+import android.content.Intent;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.dreamfutureone.milkmanui.R;
 import com.dreamfutureone.milkmanui.databinding.ActivityCreateAccountBinding;
+import com.dreamfutureone.milkmanui.ui.products.ProductsActivity;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -15,5 +18,14 @@ public class CreateAccountActivity extends AppCompatActivity {
 //        setContentView(R.layout.activity_create_account);
         binding = ActivityCreateAccountBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ProductsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
